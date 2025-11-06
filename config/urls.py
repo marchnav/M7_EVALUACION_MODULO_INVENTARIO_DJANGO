6aeb7ede1628ec1b67bb2a617511c4407e4e7377
Marcelo Navarrete <marcelonavarretey@gmail.com>
2025-11-06 15:23:11 -0300

    Se incluyen imágenes de la interfaz

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("inventario.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),  # login/logout/password
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
+
+# Servir archivos subidos (MEDIA) en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
